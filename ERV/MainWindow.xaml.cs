33ca6a6930a9f65aafeb6e0b39354759732d0ca5
tinoti <valentino.tomasic@gmail.com>
2019-03-21 22:07:36 +0100

    Add select month calendar functionality

--- ERV/MainWindow.xaml.cs
+++ ERV/MainWindow.xaml.cs
@@ -25,5 +25,32 @@ namespace ERV
 			InitializeComponent();
 		}
 
+		//Custom properties for calendar
+		private void SelectMonthCalendar_Loaded(object sender, RoutedEventArgs e)
+		{
+			//Default selected month is the previouse month
+			SelectMonthCalendar.DisplayDate = DateTime.Now.AddMonths(-1);
+		}
+
+		//Overides the default display mode, so that when the user clicks on a months it selects it and stays in year mode
+		private void SelectMonthCalendar_DisplayModeChanged(object sender, CalendarModeChangedEventArgs e)
+		{
+			//If the display mode should go to month (meaning the user clicked the month), revert it back to year mode
+			if (SelectMonthCalendar.DisplayMode == CalendarMode.Month)
+			{
+				SelectMonthCalendar.DisplayMode = CalendarMode.Year;
+			}
+		}
+
+		//Overides the default behaviour. The highlight stays on the clicked month and changes only when you click another month
+		private void SelectMonthCalendar_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			//Sets the mouse capture to null before it gets to child element (thats why we use PreviewMouseUp instead of MouseUp) so it can't highlight anything and the highlight stays
+			//on the clicked month
+			if (Mouse.Captured is System.Windows.Controls.Primitives.CalendarItem)
+			{
+				Mouse.Capture(null);
+			}
+		}
 	}
 }
